c8c9b979edfb67bc7af00faf980340069c76226b
thh0003 <thh0003@mix.wvu.edu>
2020-01-24 09:36:09 -0600

    Initial commit

---
+++ tree.js
@@ -0,0 +1,48 @@
+class tree {
+    /**
+     * 
+     * "CODEID":"8456",
+       "CODEParentID":null,
+       "CODEVal":"8456",
+       "CODETxt":"(Root)",
+       "CODEIcon":"folder"
+     */
+    constructor(treeObj, locationObj){
+      
+      this.CODEID = nodeObj.CODEID;
+      this.CODEParentID = nodeObj.CODEParentID;
+      this.CODEVal = nodeObj.CODEVal;
+      this.CODETxt = nodeObj.CODETxt;
+      this.CODEIcon = nodeOBj.CODEIcon;
+      this.treeEdges = [new treeEdge(this.CODEID, this.CODEParentID) ];
+      this.nodeType = (!this.CODEParentID)?'ROOT':'NODE';
+      this.canvasNode = canvasNode(locationObj);
+    }
+
+    getCodeID (){
+        return this.CODEID;
+    }
+
+    getCodeParentID (){
+        return this.CODEParentID;
+    }
+
+    getCodeVal (){
+        return this.CODEVal;
+    }
+
+    getCodeTxt (){
+        return this.CODETxt;
+    }
+
+    getCodeIcon (){
+        return this.CODEIcon;
+    }
+
+    renderNode(){}
+
+    displayValue(){}
+
+    
+}
+module.exports = tree;
